17a6dce75501cc4929d88805c6851d4dc9d00c6e
Theebika6 <82008678+Theebika6@users.noreply.github.com>
2024-02-28 15:35:51 -0500

    created a tsx file for vehicle_details page

---
+++ app/src/app/vehicle_details.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VehicleDetails: React.FC = () => {
+  return (
+    <div>
+      <h1>Vehicle Details</h1>
+      {/* Add your layout and components for the vehicle details page here */}
+    </div>
+  );
+};
+
+export default VehicleDetails;
