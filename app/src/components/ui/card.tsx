c4424356a040702e335e479b4478e09402f15233
Shamiivan <shamiivan@gmail.com>
2024-02-28 13:36:03 -0500

    refactored card component

--- app/src/components/ui/card.tsx
+++ app/src/components/ui/card.tsx
@@ -1,4 +1,11 @@
+import Link from "next/link";
 import React from "react";
+import {
+  IoArrowForward,
+  IoLocationSharp,
+  IoTimeOutline,
+} from "react-icons/io5";
+import { Button } from "./button";
 
 interface Props {
   name: string;
@@ -7,42 +14,44 @@ interface Props {
   automatic: boolean;
   nPeople: number;
   nBags: number;
-  buttonText: string; // example is "make reservation"
 }
 
 const Card: React.FC<Props> = (
-  { name, price, description, automatic, nPeople, nBags, buttonText },
+  { name, price, description, automatic, nPeople, nBags },
 ) => {
   return (
-    <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-      <div className="relative overflow-hidden bg-cover bg-no-repeat">
+    <div className="w-72 sm:w-80 flex flex-col justify-start bg-white border overflow-hidden rounded-xl shadow-md">
+      <div className="w-full h-48 sm:h-52 bg-slate-400 rounded-t-xl">
+        <div className="absolute w-32 p-1 bg-sky-400 rounded-tl-xl rounded-br-lg text-white">
+          category
+        </div>
         <img
-          className="rounded-t-lg"
-          src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg"
-          alt=""
+          src="https://picsum.photos/200/300?grayscale"
+          // src={`/api/uploads/${encodeURIComponent(image)}`}
+          // alt={name}
+          className="w-full object-cover overflow-hidden max-h-full"
         />
-        <a href="#!">
-          <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
-          </div>
-        </a>
       </div>
-      <div className="p-6">
-        <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-          {name} - {nPeople} People, {nBags} Bags{" "}
-          {automatic ? "(Automatic)" : "(Manual)"}
-        </h5>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          {description}
-        </p>
-        <p className="mb-4 font-bold">
-          Price: {price}
-        </p>
-        <button
-          type="button"
-          className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-        >
-          {buttonText}
-        </button>
+      <div className="w-full flex flex-col px-4 pb-4 gap-2 sm-gap-3 text-start rounded-b-xl">
+        <div>
+          <h3 className="w-full text-title-l font-semibold font-raleway text-black">
+            Name
+          </h3>
+          <p className="italic text-base font-light">Additional tags</p>
+        </div>
+
+        <div className="flex flex-row items-start sm:items-center gap-2 sm:gap-6">
+          <div className="flex flex-row items-center text-sm gap-1 text-gray-600">
+            <IoTimeOutline size={24} color="gray" /> date
+          </div>
+          <div className="flex flex-row items-center text-sm gap-1 text-gray-600">
+            <IoLocationSharp size={24} color="gray" /> location
+          </div>
+        </div>
+
+        <p className="text-base text-gray-600 line-clamp-3">{description}</p>
+
+        <Button>Make a reservation</Button>
       </div>
     </div>
   );
