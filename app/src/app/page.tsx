ed68b9bddd155b63fed46b1f68e123a670fcf838
Shamiivan <shamiivan@gmail.com>
2024-02-29 13:09:23 -0500

    feat(routing) Add index page containing all the card for a vehicle

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,32 +1,13 @@
 import "@/styles/global.css";
-import Card from "@/components/ui/card";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
+import VehiclesIndex from "./vehicles";
 
 export default function Home() {
-  const cardData = {
-    name: "Car Name",
-    price: 99,
-    description:
-      "This is a great car with excellent features for your next trip.",
-    automatic: true,
-    nPeople: 4,
-    nBags: 2,
-  };
   return (
     <main>
       <Navbar />
-      <div className="bg-secondary">
-        <h1 className="text-primary-foreground">Welcome to My Page</h1>
-      </div>
-      <Card
-        name={cardData.name}
-        price={cardData.price}
-        description={cardData.description}
-        automatic={cardData.automatic}
-        nPeople={cardData.nPeople}
-        nBags={cardData.nBags}
-      />{" "}
+      <VehiclesIndex />
       <Footer />
     </main>
   );
