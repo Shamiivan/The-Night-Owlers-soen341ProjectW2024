7843dd9725baf04b7a5ad55b9136b94d457ee7b5
Shamiivan <shamiivan@gmail.com>
2024-03-01 18:12:26 -0500

    fix(database) Remove the import from db connect

--- app/src/pages/api/ping.ts
+++ app/src/pages/api/ping.ts
@@ -1,11 +1,14 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { connectToDatabase } from '../../../utils/dbConnect';
+import type { NextApiRequest, NextApiResponse } from "next";
+// import { connectToDatabase } from '../../../utils/dbConnect';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
- try {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
     await connectToDatabase();
-    res.status(200).json({ message: 'Connected to MongoDB!' });
- } catch (error) {
-    res.status(500).json({ error: 'Failed to connect to MongoDB.' });
- }
+    res.status(200).json({ message: "Connected to MongoDB!" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to connect to MongoDB." });
+  }
 }
