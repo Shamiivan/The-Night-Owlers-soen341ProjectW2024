a86e12aa53c51118271bc3eae5edc90466ab8c38
Shamiivan <shamiivan@gmail.com>
2024-03-01 16:23:39 -0500

    feat(routing) Add routing to test the db connection

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,13 +1,12 @@
 import "@/styles/global.css";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
-import VehiclesIndex from "./vehicles";
+import VehiclesIndex from "./pages/vehicles/vehicles";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <VehiclesIndex />
       <Footer />
     </main>
   );
