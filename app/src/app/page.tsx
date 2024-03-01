3ac8ffd943fd45a320327092fbeb5c4c41996f66
Shamiivan <shamiivan@gmail.com>
2024-03-01 18:20:44 -0500

    fix(pages) Remove declaration of vehicles

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,6 @@
 import "@/styles/global.css";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
-import VehiclesIndex from "./pages/vehicles/vehicles";
 
 export default function Home() {
   return (
