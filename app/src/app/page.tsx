3a942c908cb032eafc2b5d0cd12835bae65fa855
Jutipong <p.jutipong13@gmail.com>
2024-03-03 17:50:46 -0500

    delete debug code

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,6 @@
 import "@/styles/global.css";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
-import DeleteAccount from "@/components/ui/deleteAccount";
 
 export default function Home() {
   return (
