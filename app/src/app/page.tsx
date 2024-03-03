624e46597f31b427466f0b78293cfe581104d3c4
Jutipong <p.jutipong13@gmail.com>
2024-03-03 17:45:55 -0500

    Implement Delete Account Popup

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,6 +1,7 @@
 import "@/styles/global.css";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
+import DeleteAccount from "@/components/ui/deleteAccount";
 
 export default function Home() {
   return (
