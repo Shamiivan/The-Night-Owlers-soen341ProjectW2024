926e8fc86687fb567293539e2a6ad7e451ea61f9
Shamiivan <shamiivan@gmail.com>
2024-03-04 18:46:45 -0500

    feat(dashboard) Add the dashboard

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,13 +1,11 @@
 import "@/styles/global.css";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
-import DashboardComponent from "@/components/Dashboard/Dashboard";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <DashboardComponent />
       <Footer />
     </main>
   );
--- app/src/components/dashboard/nav.tsx
+++ app/src/components/dashboard/nav.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import Link from "next/link"
 import { LucideIcon } from "lucide-react"
 
