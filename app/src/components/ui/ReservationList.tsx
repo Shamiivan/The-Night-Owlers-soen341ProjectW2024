cd7bdea2e539742360331660336c332fbadc4a2e
Jutipong <p.jutipong13@gmail.com>
2024-03-03 19:39:46 -0500

    Implemented the Reservation List page and creating a UI for displaying a list of reserved cars.

---
+++ app/src/components/ui/ReservationList.tsx
@@ -0,0 +1,66 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+
+interface Reservation {
+    img: string;
+    id: number;
+    car: string;
+    startDate: string;
+    endDate: string;
+    location: string;
+}
+
+interface ReservationListProps {
+    reservationsData: Reservation[];
+}
+
+const ReservationList: React.FC<ReservationListProps> = ({ reservationsData }) => {
+    const [reservations, setReservations] = useState<Reservation[]>([]);
+
+    useEffect(() => {
+        setReservations(reservationsData);
+    }, [reservationsData]);
+
+    return (
+    <div className="container mx-auto mt-8 border-2 border-black bg-slate-100">
+      {reservations.length === 0 ? (
+        <p>No reservations found.</p>
+      ) : (
+        <div>
+          {reservations.map((reservation) => (
+            <Link key={reservation.id} href={`/`}>
+                {/* Replace the href with the actual link when available */}
+                {/* href={`/reservation-summary/${reservation.id}`} */}
+                <div className="p-4 grid grid-cols-6 gap-4 cursor-pointer">
+                  <img src={reservation.img} alt='car' className="w-full h-32 object-cover mb-4" />
+                  <div className="col-span-2 bg-slate-200 p-4 rounded-md">
+                    <p className=" text-xl font-medium">{reservation.car}</p>
+                    <p>Add more detail</p>
+                  </div>
+                  <div className="col-span-3 grid grid-row-2">
+                    <div className='pl-10 bg-slate-200 p-4 mb-2 rounded-md'>
+                      <p className='text-xl font-medium'>Location:</p>
+                      <p>{reservation.location}</p>
+                    </div>
+                    <div className='grid grid-cols-2 pl-10 bg-slate-200 p-4 rounded-md'>
+                      <div>
+                        <p className='text-xl font-medium'>Start Date:</p>
+                        <p>{reservation.startDate}</p>
+                      </div>
+                      <div>
+                        <p className='text-xl font-medium'>End Date:</p>
+                        <p>{reservation.endDate}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ReservationList;
---
+++ app/src/pages/ViewReservation/ReservationList.tsx
@@ -0,0 +1,43 @@
+import "@/styles/global.css";
+import Navbar from "@/components/ui/Navbar";
+import Footer from "@/components/ui/Footer";
+import ReservationList from "@/components/ui/ReservationList";
+
+export default function Home() {
+  const user = {
+    name: 'Jame connor',
+    UserID: 10
+  }
+  const reservationsDataSets = [
+    [
+      { img:'', id: 1, car: 'Car A', startDate: '2024-03-05', endDate: '2024-03-08', location:'Airport'},
+      // Add more reservation data for the first list
+    ],
+    [
+      { img:'', id: 2, car: 'Car B', startDate: '2024-03-10', endDate: '2024-03-15', location:'Train station' },
+      // Add more reservation data for the second list
+    ],
+    // Add more arrays for additional lists
+  ];
+
+
+  return (
+    <main>
+      <Navbar />
+      <div className="container mx-auto my-8 p-4">
+        <div className="flex items-end mb-4">
+          <p className="text-4xl font-bold mr-2">Reservation list</p>
+          <p>(click on the list for more information)</p>
+        </div>
+        <p className="text-3xl font-semibold mb-6">{user.name}</p>
+
+        {reservationsDataSets.map((reservationsData, index) => (
+          <div key={index} className="mb-8">
+            <ReservationList reservationsData={reservationsData} />
+          </div>
+        ))}
+      </div>
+      <Footer />
+    </main>
+  );
+}
