0829d88bb0d7b95ffca4e3971400fe1b235abe04
Jutipong <p.jutipong13@gmail.com>
2024-03-08 12:36:10 -0500

    fixing pipeline error

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,8 +1,9 @@
 import "@/styles/global.css";
-import Navbar from "@/components/ui/Navbar";
-import Footer from "@/components/ui/Footer";
-import { getAllUsers } from "@/utils/userRepository";
-import { IUser } from "@/models/User";
+import React from 'react';
+import Navbar from "../components/ui/Navbar";
+import Footer from "../components/ui/Footer";
+import { getAllUsers } from "../utils/userRepository";
+import { IUser } from "../models/User";
 
 async function fetchUsers() {
   const result = await getAllUsers();
@@ -15,7 +16,9 @@ async function fetchUsers() {
 export default async function Home() {
   const result = await fetchUsers();
   const users = Array.isArray(result) ? result : [];
-  console.log(users[0]._id?.toString());
+  if (users.length > 0 && users[0]._id) {
+    console.log(users[0]._id.toString());
+  }
   return (
     <main>
       <Navbar />
