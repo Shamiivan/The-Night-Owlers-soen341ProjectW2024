40f9796a9578648a23eba243db182a80bb7c955b
Jutipong <p.jutipong13@gmail.com>
2024-03-08 23:27:49 -0500

    Modify package.json and tsconfig.json

---
+++ app/src/pages/tests/users.test.jsx
@@ -0,0 +1,23 @@
+// users.test.js
+
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen, waitFor  } from '@testing-library/react';
+import Users from '@/app/admin/users/page';
+
+describe('Users Component', () => {
+  it('should handle tab switch', async () => {
+    render(<Users />);
+    
+    // Verify that the component renders with the default layout
+    expect(screen.getByText('Dashboard')).toBeInTheDocument();
+    expect(screen.getByText('Users')).toBeInTheDocument();
+    expect(screen.getByText('Vehicles')).toBeInTheDocument();
+    expect(screen.getByText('Reservations')).toBeInTheDocument();
+    expect(screen.getByText('Settings')).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.getByText('John Doe')).toBeInTheDocument(); 
+    });
+  });
+});
