4d921a1c5fe914c365ba878d760d9fd6d5ba87b0
Shamiivan <shamiivan@gmail.com>
2024-03-10 11:36:18 -0400

    Refactor authentication code and remove unused files

--- app/src/utils/auth.ts
+++ app/src/utils/auth.ts
@@ -3,6 +3,8 @@ import type { NextAuthOptions  } from "next-auth";
 import Providers from 'next-auth/providers'
 import CredentialsProvider from 'next-auth/providers/credentials' 
 import { authenticate } from '@/actions/authetication'
+import User, { IUser } from "@/models/User";
+import printError from "@/utils/print";
 
 export const authOptions = {
     providers: [
@@ -12,15 +14,20 @@ export const authOptions = {
             email: { label: "Email", type: "text", placeholder: "jsmith@example.com" },
             password: { label: "Password", type: "password" }
           },
-          authorize: async (credentials) => {
+          authorize: async (credentials, req) => {
+            console.log(credentials);
             const { email, password } = credentials as { email: string, password: string };
-            const user = await authenticate(email, password);
-    
-            if (user) {
-              return user;
-            } else {
-              return null;
-            }
+            const user = await User?.findOne({ email : email });
+            
+
+            if(!user || user.password !== password) { 
+            printError("Invalid credentials");
+            return null;}
+
+            const res = { id : user?._id, firstName: user?.firstName, lastName: user?.lastName, email: user?.email, role: user?.role}
+            console.log(res);
+            return res
+
           }
         })
      ],
@@ -30,6 +37,8 @@ export const authOptions = {
         error: '/auth/error',
         verifyRequest: '/auth/verify-request',
      },
+     secret: process.env.NEXTAUTH_SECRET,
+
 
     
 } satisfies NextAuthOptions;
