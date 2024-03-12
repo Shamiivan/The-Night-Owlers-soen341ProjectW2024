caa6a6aa75bef014a380b17809f96c178df45c13
Shamiivan <shamiivan@gmail.com>
2024-03-11 23:54:28 -0400

    Fix Navbar link

--- app/src/components/ui/Navbar.tsx
+++ app/src/components/ui/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar: React.FC = () => {
                 Manage
               </a>
             </li>
-            {/*<li>
+            <li>
               <a href="#CheckInOut" className="block py-10 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                 Check-In/Out
               </a>
@@ -61,6 +61,5 @@ const Navbar: React.FC = () => {
       </div>
     </div>
 )};
-
 export default Navbar;
 
