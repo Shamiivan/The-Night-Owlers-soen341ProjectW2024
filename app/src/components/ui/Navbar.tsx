c5321e6d45b9bfae2c0f675af15cb2535b678a1a
Shamiivan <shamiivan@gmail.com>
2024-03-12 00:00:09 -0400

    Update Navbar link to Admin

--- app/src/components/ui/Navbar.tsx
+++ app/src/components/ui/Navbar.tsx
@@ -32,8 +32,8 @@ const Navbar: React.FC = () => {
               </a>
             </li>
             <li>
-              <a href="#CheckInOut" className="block py-10 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                Check-In/Out
+              <a href="admin/users" className="block py-10 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                Admin 
               </a>
             </li>
             <li>
