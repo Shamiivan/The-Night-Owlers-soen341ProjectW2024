1aae1cc3db625b5d831b9ab48ef62c73f9ad34f5
Jutipong <p.jutipong13@gmail.com>
2024-03-12 13:33:50 -0400

    Create back button to navigate back to admin page

--- app/src/components/dashboard/updateUserForm.tsx
+++ app/src/components/dashboard/updateUserForm.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import "@/styles/global.css";
 import { Button } from '../ui/button';
+import Link from 'next/link';
 
 
 interface userProps {
@@ -86,13 +87,19 @@ const UpdateUserForm = ({ oldFirstName,oldLastName, oldEmail, oldPassword, id}: 
           className="pl-2 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         />
       </div>
-
-      <Button
+      <div className='flex justify-evenly'>
+        <Button
         type="submit"
-        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      >
-        Update User
-      </Button>
+        className=" bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Update User
+        </Button>
+        <Link href="/admin/users">
+          <Button className=' py-2 px-4 '>
+              Back
+          </Button>
+        </Link>
+      </div>
     </form>
  );
 };
