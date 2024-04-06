dd18196ed89c987507d936d9c5546870fb31f7b0
flztarill <L1280705902@outlook.com>
2024-04-06 03:45:38 -0400

    map

--- app/src/components/ui/map.tsx
+++ app/src/components/ui/map.tsx
@@ -24,7 +24,10 @@ export function Map (){
             lat: 43.642693,
             lng: -79.3871189
         }
-
+        const montreal = {
+            lat: 45.470556,
+            lng: -73.740833
+        }
         //map options
         const mapOptions: google.maps.MapOptions = {
             center: position,
@@ -48,6 +51,35 @@ export function Map (){
         marker.addListener('click', () => {
         infoWindow.open(map, marker); // Opening info window when marker is clicked
        });
+/*
+const marker2 = new Marker({
+    map: map,
+    position: position2
+});
+// Creating an info window
+const infoWindow2 = new google.maps.InfoWindow({
+   content: "<div>Car Information: <br> Model: Toyota <br> Year: 2022</div>"
+});
+
+// Adding click event listener to the marker
+marker2.addListener('click', () => {
+infoWindow2.open(map, marker2); // Opening info window when marker is clicked
+});
+*/
+const markerMontreal = new Marker({
+    map: map,
+    position: montreal
+});
+
+// 为蒙特利尔的 marker 创建信息窗口
+const infoWindowMontreal = new google.maps.InfoWindow({
+    content: "<div>Location: Montreal</div>"
+});
+
+markerMontreal.addListener('click', () => {
+    infoWindowMontreal.open(map, markerMontreal);
+});
+
 
         }
         initMap();
